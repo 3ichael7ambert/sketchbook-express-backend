76c3a7a26fee5de889219871175f85f4fd3169a4
3ichael7ambert <mlambertbusiness@gmail.com>
2024-01-19 13:27:17 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ require('colors');
 const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 const PORT = +process.env.PORT || 3005;
 
-// Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "postgresql:///sketchbook-test"
